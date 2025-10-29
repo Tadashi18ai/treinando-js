d4d69824e0028755871ef67dc9ee06f8507786d9
Tadashi18ai <leonardotadashiokok@gmail.com>
2025-10-29 16:18:27 -0300

    treinando js , foi criado arquivo Html e JS

---
+++ main.js
@@ -0,0 +1,19 @@
+ 
+// document.write("<h1>ctrl+play</h1>")
+// //esse é o comentario de uma linha
+// /* esse é o comentario 
+//  de mutiplas linhas*/
+ 
+//  //uma declaração
+//   var  x ;
+// //mais de uma declaração
+// var y ;
+// var z ;
+// // mutiplas declarações
+// var a ; b ; c 
+// //declaração e inicialização em uma unica linha
+// var alfa = 0 ; beta= 100 ;
+ 
+var idade = 10 ;
+var maisVelho = 3 * idade;
+document.write(maisVelho);
